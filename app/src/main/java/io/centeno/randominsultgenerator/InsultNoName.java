21ab8eddc9b712a42590e13fdf83ef9480af643e
Patrick Centeno <pcenteno3492@gmail.com>
2016-04-27 16:24:10 -0400

    Added ripple to generate button

---
+++ app/src/main/java/io/centeno/randominsultgenerator/InsultNoName.java
@@ -0,0 +1,7 @@
+package io.centeno.randominsultgenerator;
+
+/**
+ * Created by patrickcenteno on 4/27/16.
+ */
+public enum InsultNoName {
+}
---
+++ app/src/main/java/io/centeno/randominsultgenerator/InsultWithName.java
@@ -0,0 +1,10 @@
+package io.centeno.randominsultgenerator;
+
+/**
+ * Created by patrickcenteno on 4/27/16.
+ */
+public enum  InsultWithName
+{
+
+
+}
