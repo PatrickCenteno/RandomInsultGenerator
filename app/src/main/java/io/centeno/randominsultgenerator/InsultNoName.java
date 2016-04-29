e3b4c5ebe7e3314c05995bd35e62d9e62a58dd82
Patrick Centeno <pcenteno3492@gmail.com>
2016-04-29 15:39:06 -0400

    Added volley library as a dependency and set up the the APICaller singleton. Also set up the String arrays for the Foaas API endpoints

--- app/src/main/java/io/centeno/randominsultgenerator/InsultNoName.java
+++ app/src/main/java/io/centeno/randominsultgenerator/InsultNoName.java
@@ -3,5 +3,27 @@ package io.centeno.randominsultgenerator;
 /**
  * Created by patrickcenteno on 4/27/16.
  */
-public enum InsultNoName {
+public class InsultNoName {
+    public static String [] data = {
+            "this",
+            "that",
+            "pink",
+            "life",
+            "thing",
+            "thanks",
+            "flying",
+            "fascinating",
+            "cool",
+            "what",
+            "because",
+            "bye",
+            "diabetes",
+            "awesome",
+            "tucker",
+            "bucket",
+            "family",
+            "zayn",
+            "mornin",
+            "retard"
+    };
 }
